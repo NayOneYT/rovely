32aed6701f6f90bd9a8e205e9caf0788021e7de6
Egor Kuznechyk <nayone.tapok@gmail.com>
2026-04-26 17:53:22 +0300

    chore(frontend): Integrate Tailwind into the application

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import './main.css'
 
 import App from './App.vue'
 import router from './router'
