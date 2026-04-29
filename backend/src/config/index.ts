f653a34dffb039fab7ec106098becbdf193a0542
Egor Kuznechyk <nayone.tapok@gmail.com>
2026-04-29 23:25:58 +0300

    config(backend): Add databaseUrl to main configuration file

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -6,4 +6,5 @@ export const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET as string,
   clientUrl: process.env.CLIENT_URL as string,
+  databaseUrl: process.env.DATABASE_URL as string
 } 
