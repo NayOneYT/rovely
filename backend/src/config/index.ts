774afdc796f6439dc6e3e98af4b35a88a360d179
Egor Kuznechyk <nayone.tapok@gmail.com>
2026-05-24 00:18:13 +0300

    feat(backend): add telegram bot token

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -9,5 +9,6 @@ export const config = {
   databaseUrl: process.env.DATABASE_URL as string,
   jwtAccessSecret: process.env.JWT_ACCESS_SECRET as string,
   jwtRefreshSecret: process.env.JWT_REFRESH_SECRET as string,
-  resendApiKey: process.env.RESEND_API_KEY as string
+  resendApiKey: process.env.RESEND_API_KEY as string,
+  telegramBotToken: process.env.TELEGRAM_BOT_TOKEN as string
 } 
