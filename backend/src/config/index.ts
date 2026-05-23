82e660ea838cd4e056f1a9d65a6d80b2fdbddc77
Egor Kuznechyk <nayone.tapok@gmail.com>
2026-05-24 00:01:25 +0300

    feat(backend): add resend api key

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -8,5 +8,6 @@ export const config = {
   clientUrl: process.env.CLIENT_URL as string,
   databaseUrl: process.env.DATABASE_URL as string,
   jwtAccessSecret: process.env.JWT_ACCESS_SECRET as string,
-  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET as string
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET as string,
+  resendApiKey: process.env.RESEND_API_KEY as string
 } 
