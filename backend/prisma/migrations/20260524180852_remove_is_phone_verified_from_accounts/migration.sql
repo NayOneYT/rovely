d226559bcfd4f65fee98fc847e79bcf513b71544
Egor Kuznechyk <nayone.tapok@gmail.com>
2026-05-24 21:09:42 +0300

    refactor(backend): remove is_phone_verified from accounts

---
+++ backend/prisma/migrations/20260524180852_remove_is_phone_verified_from_accounts/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `is_phone_verified` on the `accounts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "accounts" DROP COLUMN "is_phone_verified";
