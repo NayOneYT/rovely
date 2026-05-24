554fa884e873f2a0af482dbfa6a76611c5bd01f7
Egor Kuznechyk <nayone.tapok@gmail.com>
2026-05-24 13:28:58 +0300

    feat(backend): add session middleware to telegram bot instance

--- backend/src/lib/telegramBot.ts
+++ backend/src/lib/telegramBot.ts
@@ -1,4 +1,12 @@
-import { Bot } from "grammy"
+import { Bot, Context, session, type SessionFlavor } from "grammy"
 import { config } from "@/config/index.js"
 
-export const bot = new Bot(config.telegramBotToken)
+interface SessionData {
+  isStartWriten: boolean
+}
+
+type MyContext = Context & SessionFlavor<SessionData>
+
+export const bot = new Bot<MyContext>(config.telegramBotToken)
+
+bot.use(session({ initial: (): SessionData => ({ isStartWriten: false }) }))
