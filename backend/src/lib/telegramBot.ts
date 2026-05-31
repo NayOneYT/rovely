2e135fbe8fe7e5a129d25542a947bd42dee9995b
Egor Kuznechyk <nayone.tapok@gmail.com>
2026-05-31 12:55:14 +0300

    Revert "feat(backend): add session middleware to telegram bot instance"
    This reverts commit 72ec6aa50ad2e9621255bd3b894aa5d62d6c2766.

--- backend/src/lib/telegramBot.ts
+++ backend/src/lib/telegramBot.ts
@@ -1,12 +1,4 @@
-import { Bot, Context, session, type SessionFlavor } from "grammy"
+import { Bot } from "grammy"
 import { config } from "@/config/index.js"
 
-interface SessionData {
-  isStartWriten: boolean
-}
-
-type MyContext = Context & SessionFlavor<SessionData>
-
-export const bot = new Bot<MyContext>(config.telegramBotToken)
-
-bot.use(session({ initial: (): SessionData => ({ isStartWriten: false }) }))
+export const bot = new Bot(config.telegramBotToken)
