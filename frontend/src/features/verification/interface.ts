c62af242317ecb4284fc49dd3d3dc7be33b91d45
Egor Kuznechyk <nayone.tapok@gmail.com>
2026-06-04 17:09:40 +0300

    refactor(frontend): move the type for the server response with a message

--- frontend/src/features/verification/interface.ts
+++ frontend/src/features/verification/interface.ts
@@ -1,14 +1,3 @@
-export interface VerifyResponseDto {
-  type: string
-  message: string
-}
-
 export interface CheckResponseDto {
   verified: boolean
-}
-
-export interface SendResponseDto {
-  type: string
-  message: string
-  secondsLeft?: number
 }
--- frontend/src/interface.ts
+++ frontend/src/interface.ts
@@ -2,3 +2,9 @@ export interface ResponseErrorDto {
   message?: string
   errors?: Record<string, string>
 }
+
+export interface ResponseMessageDto {
+  type: "success" | "info" | "warning" | "error"
+  message: string
+  secondsLeft?: number
+}
