1cdc964b1c21660a6e6e96095e72e9761baa006c
Egor Kuznechyk <nayone.tapok@gmail.com>
2026-06-09 22:38:02 +0300

    feat(frontend): add GoogleAuth composable and connect Google to the application

---
+++ frontend/src/features/auth/composables/useGoogleAuth.ts
@@ -0,0 +1,32 @@
+import { config } from "@/config"
+import api from "../api"
+import { AxiosError } from "axios"
+import { toast } from "vue-sonner"
+import type { Ref } from "vue"
+
+export const useGoogleAuth = (isProcessing: Ref<boolean>) => {
+  const authenticate = async (response: google.accounts.oauth2.CodeResponse) => {
+    if (response.error) {
+      isProcessing.value = false
+      return
+    }
+    try {
+      await api.google(response.code)
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message ?? "Произошла ошибка сервера, попробуйте позже")
+      }
+    } finally {
+      isProcessing.value = false
+    }
+  }
+
+  const googleClient = google.accounts.oauth2.initCodeClient({
+    client_id: config.googleClientId,
+    scope: 'openid profile email',
+    ux_mode: 'popup',
+    callback: authenticate
+  })
+
+  return { googleClient }
+}
