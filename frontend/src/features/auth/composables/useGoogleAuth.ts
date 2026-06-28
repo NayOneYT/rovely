13fe9b93e9b9eec734e6f5618d54388b0cf5bba5
Egor Kuznechyk <nayone.tapok@gmail.com>
2026-06-28 22:21:44 +0300

    improvement(frontend): add logic for successful Google authorization

--- frontend/src/features/auth/composables/useGoogleAuth.ts
+++ frontend/src/features/auth/composables/useGoogleAuth.ts
@@ -2,9 +2,14 @@ import { config } from "@/config"
 import api from "../api"
 import { AxiosError } from "axios"
 import { toast } from "vue-sonner"
+import { useRouter } from "vue-router"
+import { useLocalStorage } from "@vueuse/core"
 import type { Ref } from "vue"
 
 export const useGoogleAuth = (isProcessing: Ref<boolean>) => {
+  const router = useRouter()
+  const theUserLoggedInOnce = useLocalStorage("theUserLoggedInOnce", false)
+
   const authenticate = async (response: google.accounts.oauth2.CodeResponse) => {
     if (response.error) {
       isProcessing.value = false
@@ -12,6 +17,9 @@ export const useGoogleAuth = (isProcessing: Ref<boolean>) => {
     }
     try {
       await api.google(response.code)
+      const account = await api.me()
+      router.push(`/profiles/${account.profile.username}`)
+      theUserLoggedInOnce.value = true
     } catch (error) {
       if (error instanceof AxiosError) {
         toast.error(error.response?.data.message ?? "Произошла ошибка сервера, попробуйте позже")
