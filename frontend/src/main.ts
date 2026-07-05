39b476f6d5b1c4722e34e81eb2c4cf75c19344c7
Egor Kuznechik <nayone.tapok@gmail.com>
2026-07-05 12:50:56 +0300

    refactor(frontend): rename main.css to style.css

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { VueQueryPlugin } from '@tanstack/vue-query'
-import './main.css'
+import './style.css'
 
 import App from './App.vue'
 import router from './router'
