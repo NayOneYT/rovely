bbf28177f8dbdb54eed57025cac0abba409775ed
Egor Kuznechik <nayone.tapok@gmail.com>
2026-07-05 13:58:48 +0300

    refactor(backend): remove cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,7 @@
 import express from "express"
-import { config } from "./config/index.js"
 import authRouter from "./modules/auth/router.js"
 import { errorHandler } from "./middlewares/error.middleware.js"
 import cookieParser from "cookie-parser"
-import cors from "cors"
 import emailVerificationRouter from "./modules/verification/email/router.js"
 import phoneVerificationRouter from "./modules/verification/phone/router.js"
 
@@ -11,13 +9,6 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-
-if (config.nodeEnv !== "production") {
-  app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-  }))
-}
 
 app.use("/api/auth", authRouter)
 app.use("/api/verification/email", emailVerificationRouter)
