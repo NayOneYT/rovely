e95a8e27f398b27c17952964a79cd4ec90d882a0
Egor Kuznechik <nayone.tapok@gmail.com>
2026-07-24 18:37:50 +0300

    refactor(frontend): remove fieldErrors from the ApiError object

--- frontend/src/shared/api/types/error.ts
+++ frontend/src/shared/api/types/error.ts
@@ -3,7 +3,6 @@ import type { ErrorResponseData } from "./error-response"
 
 export class ApiError extends Error {
   public readonly code: ErrorCode
-  public readonly fieldErrors?: Record<string, string>
   [key: string]: any
 
   constructor(public data: ErrorResponseData) {
@@ -11,7 +10,6 @@ export class ApiError extends Error {
     this.name = "ApiError"
     const { code, fieldErrors, ...other } = data
     this.code = code
-    this.fieldErrors = fieldErrors
     Object.assign(this, other)
   }
 }
