76db4be772dcd6ac0553c1725178ba16691f78e6
Egor Kuznechik <nayone.tapok@gmail.com>
2026-08-09 10:34:11 +0300

    refactor(frontend): create a parent component for fields with a button

---
+++ frontend/src/shared/components/ui/fields/FieldWithButton.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { AppButton, InputError } from ".."
+const model = defineModel()
+
+const {
+  type = "text"
+} = defineProps<{
+  id: string
+  label: string
+  type?: string
+  autocomplete: string
+  maxlength: string | number
+  disabled?: boolean
+  inputDisabled?: boolean
+  buttonDisabled?: boolean
+  clientError?: string
+  serverError?: string
+}>()
+
+const emit = defineEmits<{
+  blur: []
+  click: []
+}>()
+</script>
+
+<template>
+  <label v-bind="$attrs" :for="id" class="text-sm font-medium pb-1 self-start">{{ label }}</label>
+  <div 
+    class="
+      group flex items-center w-full bg-[#070908] rounded-[13.5px] border border-[#222a27] 
+      focus-within:outline-none focus-within:border-[#13d373] focus-within:shadow-[0_0_6px_#13d373] transition-all duration-200
+    "
+  >
+    <input
+      :type
+      :autocomplete
+      :id
+      :maxlength
+      v-model="model"
+      @blur="emit('blur')"
+      :disabled="disabled || inputDisabled"
+      class="flex-1 bg-transparent px-4 py-2.75 focus-visible:outline-none"
+    >
+    <div class="w-px h-6 transition-all bg-[#222a27] group-focus-within:bg-[#13d373] group-focus-within:shadow-[0_0_6px_#13d373]" />
+    <AppButton
+      variant="icon"
+      @click="emit('click')"
+      :disabled="disabled || buttonDisabled"
+      class="m-0.75 mr-1.25"
+    >
+      <slot />
+    </AppButton>
+  </div>
+  <InputError :clientError :serverError />
+</template>
--- frontend/src/shared/components/ui/fields/PasswordField.vue
+++ frontend/src/shared/components/ui/fields/PasswordField.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { usePasswordField } from "@/shared/composables/fields"
-import { AppButton, InputError } from ".."
+import FieldWithButton from "./FieldWithButton.vue"
 import { Eye, EyeOff } from "@lucide/vue"
 
 defineProps<{
-  label?: string
   isNewPassword?: boolean
   field: ReturnType<typeof usePasswordField>
   disabled?: boolean
@@ -13,35 +12,22 @@ defineProps<{
 </script>
 
 <template>
-  <label v-bind="$attrs" for="password" class="text-sm font-medium pb-1 self-start">{{ label ?? (isNewPassword ? "Новый пароль" : "Пароль") }}</label>
-  <div 
-    class="
-      group flex items-center w-full bg-[#070908] rounded-[13.5px] border border-[#222a27] 
-      focus-within:outline-none focus-within:border-[#13d373] focus-within:shadow-[0_0_6px_#13d373] transition-all duration-200
-    "
+  <FieldWithButton
+    v-model="field.value.value"
+    id="password"
+    @click="field.showPassword.value = !field.showPassword.value"
+    @blur="field.onBlur"
+    :label="isNewPassword ? 'Новый пароль' : 'Пароль'"
+    :type="field.showPassword.value ? 'text' : 'password'"
+    :autocomplete="isNewPassword ? 'new-password' : 'current-password'"
+    maxlength="72"
+    :disabled
+    :clientError="field.clientError.value"
+    :serverError
   >
-    <input
-      :type="field.showPassword.value? 'text' : 'password'"
-      :autocomplete="isNewPassword ? 'new-password' : 'current-password'"
-      id="password"
-      maxlength="72"
-      v-model="field.value.value"
-      @blur="field.onBlur"
-      :disabled
-      class="flex-1 bg-transparent px-4 py-2.75 focus-visible:outline-none"
-    >
-    <div class="w-px h-6 transition-all bg-[#222a27] group-focus-within:bg-[#13d373] group-focus-within:shadow-[0_0_6px_#13d373]" />
-    <AppButton
-      variant="icon"
-      @click="field.showPassword.value = !field.showPassword.value"
-      :disabled
-      class="m-0.75 mr-1.25"
-    >
-      <component 
-        :is="field.showPassword.value ? Eye : EyeOff"
-        :class="field.showPassword.value ? 'size-7' : 'size-6'"
-      />
-    </AppButton>
-  </div>
-  <InputError :clientError="field.clientError.value" :serverError />
+    <component 
+      :is="field.showPassword.value ? Eye : EyeOff"
+      :class="field.showPassword.value ? 'size-7' : 'size-6'"
+    />
+  </FieldWithButton>
 </template>
