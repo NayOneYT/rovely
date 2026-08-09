a94fca4d0e562912d4609193dd9947f60ba2049f
Egor Kuznechik <nayone.tapok@gmail.com>
2026-08-09 10:07:26 +0300

    refactor(frontend): remove the wrapper div for the fields and bind the attrs to the label

--- frontend/src/shared/components/ui/fields/IdentifierField.vue
+++ frontend/src/shared/components/ui/fields/IdentifierField.vue
@@ -13,24 +13,22 @@ const {
 </script>
 
 <template>
-  <div>
-    <label for="identifier" class="text-sm font-medium pb-1 self-start">{{ label }}</label>
-    <input
-      type="text"
-      autocomplete="username"
-      id="identifier"
-      maxlength="254"
-      placeholder="NayOne | email@example.com | +375 29 123 45 67" 
-      spellcheck="false"
-      :value="field.value.value"
-      @input="field.onInput"
-      @blur="field.onBlur"
-      :disabled
-      class="
-        w-full placeholder:text-white/40 bg-[#070908] rounded-[13.5px] px-4 py-2.75 border border-[#222a27]
-        focus-visible:outline-none focus-visible:border-[#13d373] focus-visible:shadow-[0_0_6px_#13d373] transition-all duration-200
-      "
-    >
-    <InputError :clientError="field.clientError.value" :serverError />
-  </div>
+  <label v-bind="$attrs" for="identifier" class="text-sm font-medium pb-1 self-start">{{ label }}</label>
+  <input
+    type="text"
+    autocomplete="username"
+    id="identifier"
+    maxlength="254"
+    placeholder="NayOne | email@example.com | +375 29 123 45 67" 
+    spellcheck="false"
+    :value="field.value.value"
+    @input="field.onInput"
+    @blur="field.onBlur"
+    :disabled
+    class="
+      w-full placeholder:text-white/40 bg-[#070908] rounded-[13.5px] px-4 py-2.75 border border-[#222a27]
+      focus-visible:outline-none focus-visible:border-[#13d373] focus-visible:shadow-[0_0_6px_#13d373] transition-all duration-200
+    "
+  >
+  <InputError :clientError="field.clientError.value" :serverError />
 </template>
--- frontend/src/shared/components/ui/fields/PhoneField.vue
+++ frontend/src/shared/components/ui/fields/PhoneField.vue
@@ -13,22 +13,20 @@ const {
 </script>
 
 <template>
-  <div>
-    <label for="phone" class="text-sm font-medium pb-1 self-start">{{ label }}</label>
-    <input
-      type="tel"
-      autocomplete="tel"
-      id="phone"
-      placeholder="+375 29 123 45 67"
-      :value="field.value.value"
-      @input="field.onInput"
-      @blur="field.onBlur"
-      :disabled
-      class="
-        w-full placeholder:text-white/40 bg-[#070908] rounded-[13.5px] px-4 py-2.75 border border-[#222a27]
-        focus-visible:outline-none focus-visible:border-[#13d373] focus-visible:shadow-[0_0_6px_#13d373] transition-all duration-200
-      "
-    >
-    <InputError :clientError="field.clientError.value" :serverError />
-  </div>
+  <label v-bind="$attrs" for="phone" class="text-sm font-medium pb-1 self-start">{{ label }}</label>
+  <input
+    type="tel"
+    autocomplete="tel"
+    id="phone"
+    placeholder="+375 29 123 45 67"
+    :value="field.value.value"
+    @input="field.onInput"
+    @blur="field.onBlur"
+    :disabled
+    class="
+      w-full placeholder:text-white/40 bg-[#070908] rounded-[13.5px] px-4 py-2.75 border border-[#222a27]
+      focus-visible:outline-none focus-visible:border-[#13d373] focus-visible:shadow-[0_0_6px_#13d373] transition-all duration-200
+    "
+  >
+  <InputError :clientError="field.clientError.value" :serverError />
 </template>
