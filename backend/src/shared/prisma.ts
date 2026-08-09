b99509afba889918707025e6b91b5f951b91f2a0
Egor Kuznechik <nayone.tapok@gmail.com>
2026-08-09 16:35:33 +0300

    style(backend): remove all ; chars in prisma.ts

--- backend/src/shared/prisma.ts
+++ backend/src/shared/prisma.ts
@@ -1,8 +1,8 @@
 import { config } from "./config.js"
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "../generated/prisma/client.js"
 
-const adapter = new PrismaPg({ connectionString: config.databaseUrl });
-const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString: config.databaseUrl })
+const prisma = new PrismaClient({ adapter })
 
-export { prisma };
+export { prisma }
