6e131a5b2a534eef8d8c698e4fa5f14c437d8bd1
Egor Kuznechik <nayone.tapok@gmail.com>
2026-08-17 10:23:49 +0300

    fix(frontend): add -z-1 to all elements in MinimalLayout

--- frontend/src/shared/layouts/MinimalLayout.vue
+++ frontend/src/shared/layouts/MinimalLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="relative h-screen overflow-hidden flex flex-wrap content-center justify-center">
-    <div class="absolute top-1/3 -right-40 size-125 rounded-full bg-brand/3 blur-[125px]" />
-    <div class="absolute -bottom-40 left-1/4 w-175 h-100 rounded-full bg-brand/3 blur-[140px]" />
+    <div class="-z-1 absolute top-1/3 -right-40 size-125 rounded-full bg-brand/3 blur-[125px]" />
+    <div class="-z-1 absolute -bottom-40 left-1/4 w-175 h-100 rounded-full bg-brand/3 blur-[140px]" />
     <div class="-z-1 absolute inset-0 opacity-2.5 bg-grid" />
     <slot />
   </main>
