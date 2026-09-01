8838c88c10461d87afb4390eda75f6ddb603736d
Egor Kuznechik <nayone.tapok@gmail.com>
2026-09-01 15:51:04 +0300

    fix(frontend): formatMsToMMSS function now rounds seconds  up rather than down

--- frontend/src/shared/utils/formatTime.ts
+++ frontend/src/shared/utils/formatTime.ts
@@ -1,6 +1,6 @@
 export const formatMsToMMSS = (ms: number) => {
   if (ms <= 0) return "00:00"
-  const totalSec = Math.floor(ms / 1000)
+  const totalSec = Math.ceil(ms / 1000)
   const m = Math.floor(totalSec / 60)
   const s = totalSec % 60
   return `${String(m).padStart(2, "0")}:${String(s).padStart(2, "0")}`
