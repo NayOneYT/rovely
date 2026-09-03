e86a216dbbeabc0395e1a7289e17ea831244d5e4
Egor Kuznechik <nayone.tapok@gmail.com>
2026-09-03 11:32:05 +0300

    refactor(backend): move compose.test from root dir to backend

--- backend/vitest.global-setup.ts
+++ backend/vitest.global-setup.ts
@@ -3,10 +3,10 @@ import { execSync } from "child_process"
 
 export const setup = () => {
   dotenv.config({ path: ".env.test", override: true })
-  execSync("docker compose -f ../compose.test.yaml up -d", { stdio: "inherit" })
+  execSync("docker compose -f ./compose.test.yaml up -d", { stdio: "inherit" })
   execSync("npx prisma migrate deploy", { env: process.env, stdio: "inherit" })
 }
 
 export const teardown = () => {
-  execSync("docker compose -f ../compose.test.yaml down", { stdio: "inherit" })
+  execSync("docker compose -f ./compose.test.yaml down", { stdio: "inherit" })
 }
