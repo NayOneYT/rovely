081a6a9cb82d0fe03ac52d04e12eb15f39757d08
Egor Kuznechik <nayone.tapok@gmail.com>
2026-09-07 12:43:18 +0300

    test(backend): outline test cases for bot.service

---
+++ backend/src/modules/bot/bot.service.test.ts
@@ -0,0 +1,10 @@
+describe("botService", () => {
+  describe("checkLink", () => {
+    it.todo("returns false for a non-existent telegramUserId")
+    it.todo("returns true for an existing telegramUserId")
+  })
+
+  describe("saveLink", () => {
+    it.todo("creates a new telegramLink in the DB on success")
+  })
+})
