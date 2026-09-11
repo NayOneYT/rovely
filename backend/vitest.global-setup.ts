effd297b414bc78d6a6f26e20d08f7b75f53415d
Egor Kuznechik <nayone.tapok@gmail.com>
2026-09-11 20:57:52 +0300

    refactor(backend): remove useless override option for dotenv.config in vitest.global-setup

--- backend/vitest.global-setup.ts
+++ backend/vitest.global-setup.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 import { execSync } from "child_process"
 
 export const setup = () => {
-  dotenv.config({ path: ".env.test", override: true })
+  dotenv.config({ path: ".env.test" })
   execSync("docker compose -f ./compose.test.yaml up -d", { stdio: "inherit" })
   execSync("npx prisma migrate deploy", { env: process.env, stdio: "inherit" })
 }
