27505f2c28aa6bbeca1564a58654b9f68caca126
Egor Kuznechik <nayone.tapok@gmail.com>
2026-09-13 17:13:11 +0300

    refactor(backend): use inbox tsconfigPaths option instead of vite-tsconfig-paths

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vitest/config"
-import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   test: {
     globals: true,
     globalSetup: "./vitest.global-setup.ts",
     setupFiles: ["./tests/load-env.ts", "./vitest.setup.ts"]
+  },
+  resolve: {
+    tsconfigPaths: true
   }
 })
