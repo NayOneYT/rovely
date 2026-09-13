1a4604e364c1bb5c1dfe7cc7a434214654ef8b07
Egor Kuznechik <nayone.tapok@gmail.com>
2026-09-13 17:17:54 +0300

    test(backend): cleanup stdout noise during testing

--- backend/vitest.global-setup.ts
+++ backend/vitest.global-setup.ts
@@ -3,10 +3,10 @@ import { execSync } from "child_process"
 
 export const setup = () => {
   dotenv.config({ path: ".env.test", quiet: true })
-  execSync("docker compose -f ./compose.test.yaml up -d", { stdio: "inherit" })
-  execSync("npx prisma migrate deploy", { env: process.env, stdio: "inherit" })
+  execSync("docker compose -f ./compose.test.yaml up -d", { stdio: "ignore" })
+  execSync("npx prisma migrate deploy", { env: process.env, stdio: "ignore" })
 }
 
 export const teardown = () => {
-  execSync("docker compose -f ./compose.test.yaml down", { stdio: "inherit" })
+  execSync("docker compose -f ./compose.test.yaml down", { stdio: "ignore" })
 }
